3de26a30049cbbdfc8d4d781f5cd6dc68eeef915
Tuli Jakobson <turbotuli@gmail.com>
2017-04-08 20:31:24 +0000

    Project associations updated.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   has_many :users
   has_many :users, through: :contributors
+  has_many :contributors
+  has_many :users, through: :contributors
   has_many :tasks
   validates :name, presence: true
 
